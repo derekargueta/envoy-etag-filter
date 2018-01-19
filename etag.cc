cb32ef4863546a4993745e0f8d0910e95404b860
Derek Argueta <dereka@pinterest.com>
2018-01-18 18:47:51 -0800

    update envoy source and start on etag logic

--- etag.cc
+++ etag.cc
@@ -1,4 +1,5 @@
 #include <string>
+#include <iostream>
 
 #include "etag.h"
 
@@ -16,7 +17,12 @@ EtagFilter::~EtagFilter() {}
 
 void EtagFilter::onDestroy() {}
 
-FilterHeadersStatus EtagFilter::decodeHeaders(HeaderMap&, bool) {
+FilterHeadersStatus EtagFilter::decodeHeaders(HeaderMap& headers, bool) {
+  auto etag_entry = headers.get(LowerCaseString("etag"));
+  if (etag_entry != nullptr) {
+    etag_value_ = std::string(etag_entry->value().c_str());
+  }
+
   return FilterHeadersStatus::Continue;
 }
 
@@ -29,6 +35,29 @@ FilterTrailersStatus EtagFilter::decodeTrailers(HeaderMap&) {
 }
 void EtagFilter::setDecoderFilterCallbacks(StreamDecoderFilterCallbacks& callbacks) {
   decoder_callbacks_ = &callbacks;
+}
+
+FilterHeadersStatus EtagFilter::encodeHeaders(HeaderMap& headers, bool) {
+  auto etag_entry = headers.get(LowerCaseString("etag"));
+  if (etag_entry != nullptr) {
+    std::string upstream_etag(etag_entry->value().c_str());
+    if (upstream_etag == etag_value_) {
+      std::cout << "WE GOT A MATCH" << std::endl;
+      // TODO update the response and remove the body
+    }
+  }
+  return FilterHeadersStatus::Continue;
+}
+
+FilterDataStatus EtagFilter::encodeData(Buffer::Instance&, bool) {
+  return FilterDataStatus::Continue;
+}
+
+FilterTrailersStatus EtagFilter::encodeTrailers(HeaderMap&) {
+  return FilterTrailersStatus::Continue;
+}
+void EtagFilter::setEncoderFilterCallbacks(StreamEncoderFilterCallbacks& callbacks) {
+  encoder_callbacks_ = &callbacks;
 }
 
 } // Http
--- etag.h
+++ etag.h
@@ -7,7 +7,7 @@
 namespace Envoy {
 namespace Http {
 
-class EtagFilter : public StreamDecoderFilter {
+class EtagFilter : public StreamFilter {
 public:
   EtagFilter();
   ~EtagFilter();
@@ -19,8 +19,16 @@ public:
   FilterTrailersStatus decodeTrailers(HeaderMap&) override;
   void setDecoderFilterCallbacks(StreamDecoderFilterCallbacks& callbacks) override;
 
+  FilterHeadersStatus encodeHeaders(HeaderMap& headers, bool) override;
+  FilterDataStatus encodeData(Buffer::Instance&, bool) override;
+  FilterTrailersStatus encodeTrailers(HeaderMap&) override;
+  void setEncoderFilterCallbacks(StreamEncoderFilterCallbacks& callbacks) override;
+
 private:
   StreamDecoderFilterCallbacks* decoder_callbacks_{};
+  StreamEncoderFilterCallbacks* encoder_callbacks_{};
+
+  std::string etag_value_{};
 };
 
 } // Http
